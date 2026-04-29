f44d2e4b5d5ebf6c301ea47f4271c22a753f387f
oh.yongtaek <oh.yongtaek@secern.ai>
2026-04-29 13:10:58 +0900

    build: update Package.swift to v2.0.4 (Swift 6.3.1)

--- Package.swift
+++ Package.swift
@@ -31,23 +31,23 @@ let package = Package(
         ),
         .binaryTarget(
             name: "Dlite",
-            url: "https://github.com/CUBOX-Co-Ltd/dlite-sdk-ios/releases/download/2.0.3/Dlite.xcframework.zip",
-            checksum: "8983153e81db77e4d6874fb257309e768cbf5ca92d3c2e9243f80d51374253fb"
+            url: "https://github.com/CUBOX-Co-Ltd/dlite-sdk-ios/releases/download/2.0.4/Dlite.xcframework.zip",
+            checksum: "d17a55223a1fa0a6c7d8706863937e4e1587fd0f9ac33c6de50efe87d459ccaa"
         ),
         .binaryTarget(
             name: "Cameramodule",
-            url: "https://github.com/CUBOX-Co-Ltd/dlite-sdk-ios/releases/download/2.0.3/Cameramodule.xcframework.zip",
-            checksum: "82e8624a99c4200c0fce0e74663779517043e1974e428170c988c3212d29d2b9"
+            url: "https://github.com/CUBOX-Co-Ltd/dlite-sdk-ios/releases/download/2.0.4/Cameramodule.xcframework.zip",
+            checksum: "9e0bdc932173a8cece70b8c17ef4ad75556223489785b926b7f5b5e335493b6b"
         ),
         .binaryTarget(
             name: "Inferencemodule",
-            url: "https://github.com/CUBOX-Co-Ltd/dlite-sdk-ios/releases/download/2.0.3/Inferencemodule.xcframework.zip",
-            checksum: "fc4d3a7832431758d44fbcc4c270c7113527b212084c7bba33618d0134329e88"
+            url: "https://github.com/CUBOX-Co-Ltd/dlite-sdk-ios/releases/download/2.0.4/Inferencemodule.xcframework.zip",
+            checksum: "b961182635b012623dc1ddc67dcbc2d62d5edb20f09f9f476b29a8da77b202ee"
         ),
         .binaryTarget(
             name: "LicenseModule",
-            url: "https://github.com/CUBOX-Co-Ltd/dlite-sdk-ios/releases/download/2.0.3/LicenseModule.xcframework.zip",
-            checksum: "cbcedae3206a7420d31c9a0e74f56aed72c59b29cb99d6092683fd7d0f7ff068"
+            url: "https://github.com/CUBOX-Co-Ltd/dlite-sdk-ios/releases/download/2.0.4/LicenseModule.xcframework.zip",
+            checksum: "99e15a5ccf04ebe52a46781fe392749b697418ce876143fe68fb9eeb652e08a9"
         ),
     ]
 )
